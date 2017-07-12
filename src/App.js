54f9b2edabf37fe18367986b8c6a67c6593c9242
Marissa Tamaki <tamaki.marissa@gmail.com>
2017-07-11 23:54:00 -0700

    Add white box

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import './App.css';
 import Pomomaki from './Pomomaki'
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//       </div>
-//     );
-//   }
-// }
-
 // App Component should remain a littlel more specialized and exist really
 // only to contain our application itself 
 
@@ -18,7 +9,6 @@ const App = () => {
   return (
     <div className="App">
       <Pomomaki name="maki"/>
-      <Pomomaki name="marinu"/>
     </div>
   );
 };
